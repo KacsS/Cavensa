a256c5cff0325a725a5bfbfb513041d15e81c6c8
KacsS <kennercadenas@gmail.com>
2023-02-18 22:01:24 -0400

    first commit

---
+++ Atssets/JavaScript/funciones.js
@@ -0,0 +1,18 @@
+let menu = document.querySelector("#menu-icon i");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    menu.classList.toggle("fa-times")
+    navbar.classList.toggle("open")
+  
+    
+}
+
+function imgSlider(anything){
+    document.querySelector('.imagenM').src = anything;
+}
+
+function cambioColor(color){
+    const circulo = document.querySelector('.circulo');
+    circulo.style.background = color;
+}
